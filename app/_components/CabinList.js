449158e39f79517ce2405cd9b84c7dd12f23f457
HelderLessa <helderlssande@gmail.com>
2025-04-30 08:51:49 -0300

    Initial commit

---
+++ app/_components/CabinList.js
@@ -0,0 +1,40 @@
+// import { unstable_noStore as noStore } from "next/cache";
+
+import CabinCard from "@/app/_components/CabinCard";
+import { getCabins } from "../_lib/data-service";
+
+export const CabinList = async ({ filter }) => {
+  // noStore();
+
+  const cabins = await getCabins();
+
+  if (!cabins.length) return null;
+
+  let displayedCabins;
+  switch (filter) {
+    case "all":
+      displayedCabins = cabins;
+      break;
+    case "small":
+      displayedCabins = cabins.filter((cabin) => cabin.maxCapacity <= 3);
+      break;
+    case "medium":
+      displayedCabins = cabins.filter(
+        (cabin) => cabin.maxCapacity >= 4 && cabin.maxCapacity <= 7
+      );
+      break;
+    case "large":
+      displayedCabins = cabins.filter((cabin) => cabin.maxCapacity >= 8);
+      break;
+    default:
+      displayedCabins = cabins;
+      break;
+  }
+  return (
+    <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
+      {displayedCabins.map((cabin) => (
+        <CabinCard cabin={cabin} key={cabin.id} />
+      ))}
+    </div>
+  );
+};
